ac351ba674419d95008fb855f8c0fe5c5d1339be
Ethan Lee <Howisitgoing@yeah.net>
2021-02-10 20:33:32 +0800

    add weight matrix preprocessing method for backup.

--- data/loader.py
+++ data/loader.py
@@ -25,6 +25,14 @@ class Utils:
         x = z * Utils.std + Utils.mean
         return x
 
+    @staticmethod # 存档，可能在其他项目用
+    def weight_matrix_preprocessing(file_path, normalized_k=0.1):
+        dist_mat = pd.read_csv(file_path, header=None).to_numpy()
+        std = np.std(dist_mat.flatten())
+        adj_mat = np.exp(-np.square(dist_mat / std))
+        adj_mat[adj_mat < normalized_k] = 0
+        return adj_mat
+
 
 """以下代码针对已经处理好的PeMS(228)数据集"""
 

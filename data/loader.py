9f4f19ec478b300dcc856f264318d4c0b5045bf7
Yuexin Li <meetyuexinli@163.com>
2021-03-28 15:36:56 +0800

    70% for training. 20% for testing. 10% for validation.

--- data/loader.py
+++ data/loader.py
@@ -35,20 +35,20 @@ class Utils:
 
 
 class TrafficFlowData:
-    def __init__(self, device: torch.device, v_path: str, w_path: str, len_train: int, len_val: int,
-                 in_timesteps: int = 12, out_timesteps: int = 3):
+    def __init__(self, device: torch.device, v_path: str, w_path: str, train_pct: float, test_pct: float,
+                 n_time: int = 12, out_time: int = 3):
         self.device = device
         self.v_path = v_path
         self.w_path = w_path
-        self.len_train = len_train
-        self.len_val = len_val
-        self.in_timesteps = in_timesteps
-        self.out_timesteps = out_timesteps
+        self.train_pct = train_pct
+        self.test_pct = test_pct
+        self.n_time = n_time
+        self.out_time = out_time
         self.train = None
         self.val = None
         self.test = None
         self.w_adj_mat = self.get_weighted_adjacency_matrix()
-        self.num_nodes = self.w_adj_mat.shape[1]
+        self.n_node = self.w_adj_mat.shape[1]
         self.gen_data()
 
     def get_weighted_adjacency_matrix(self, sigma2=0.1, epsilon=0.5, scaling=True):
@@ -57,24 +57,27 @@ class TrafficFlowData:
 
     def get_data(self):
         df = pd.read_csv(self.v_path, header=None)
-        train_data = df[: self.len_train].to_numpy()
-        val_data = df[self.len_train: self.len_train + self.len_val].to_numpy()
-        test_data = df[self.len_train + self.len_val:].to_numpy()
-        return train_data, val_data, test_data
+        len_df = len(df)
+        len_train = int(self.train_pct * len_df)
+        len_test = int(self.test_pct * len_df)
+        train_data = df[: len_train].to_numpy()
+        test_data = df[len_train: len_train + len_test].to_numpy()
+        val_data = df[len_train + len_test:].to_numpy()
+        return train_data, test_data, val_data
 
     def transform_data(self, data: np.ndarray):
         # transform from row data to formatted data
         len_record = len(data)
-        num_available_data = len_record - self.in_timesteps - self.out_timesteps
+        num_available_data = len_record - self.n_time - self.out_time
 
-        x = np.zeros([num_available_data, self.in_timesteps, self.num_nodes, 1])
-        y = np.zeros([num_available_data, self.num_nodes])
+        x = np.zeros([num_available_data, self.n_time, self.n_node, 1])
+        y = np.zeros([num_available_data, self.n_node])
 
         for i in range(num_available_data):
             start = i
-            end = i + self.in_timesteps
-            x[i, :, :, :] = data[start: end].reshape(self.in_timesteps, self.num_nodes, 1)
-            y[i] = data[end + self.out_timesteps - 1]
+            end = i + self.n_time
+            x[i, :, :, :] = data[start: end].reshape(self.n_time, self.n_node, 1)
+            y[i] = data[end + self.out_time - 1]
 
         return x, y
 
@@ -91,15 +94,14 @@ class TrafficFlowData:
         val_data_y = torch.Tensor(Utils.z_score(val_data_y)).to(self.device)
         test_data_x = torch.Tensor(Utils.z_score(test_data_x)).to(self.device)
         test_data_y = torch.Tensor(Utils.z_score(test_data_y)).to(self.device)
-        # test_data_y = torch.Tensor(test_data_y).to(self.device)
 
         self.train = TensorDataset(train_data_x, train_data_y)
         self.val = TensorDataset(val_data_x, val_data_y)
         self.test = TensorDataset(test_data_x, test_data_y)
 
     def get_conv_kernel(self, approx: str):
-        if approx == "Linear":
-            W_wave = np.eye(self.num_nodes, self.num_nodes) + self.w_adj_mat
+        if approx == "linear":
+            W_wave = np.eye(self.n_node, self.n_node) + self.w_adj_mat
             D_wave = np.diag(np.sum(W_wave, axis=1))
             kernel = np.matmul(
                 np.matmul(fractional_matrix_power(D_wave, -0.5), W_wave),
@@ -116,7 +118,7 @@ class TrafficFlowData:
                         v.append(kernel[i, j])
             index = torch.LongTensor([r, c])
             value = torch.FloatTensor(v)
-            sparse_kernel = torch.sparse.FloatTensor(index, value, [self.num_nodes, self.num_nodes])
+            sparse_kernel = torch.sparse.FloatTensor(index, value, [self.n_node, self.n_node])
             return sparse_kernel.to(self.device)
         elif approx == "Cheb":
             pass
--- main.py
+++ main.py
@@ -14,16 +14,14 @@ parser.add_argument('--batch_size', type=int, default=50)
 parser.add_argument('--epochs', type=int, default=50)
 parser.add_argument('--decay_rate', type=float, default=0.7)
 parser.add_argument('--decay_steps', type=int, default=5)
-parser.add_argument('--opt', type=str, default='RMSProp')
 parser.add_argument('--approx', type=str, default='linear')
-parser.add_argument('--v_path', type=str, default='./data/PeMS_V_228.csv')
-parser.add_argument('--w_path', type=str, default='./data/PeMS_W_228.csv')
+parser.add_argument('--v_path', type=str, default='./data/v_pems_228.csv')
+parser.add_argument('--w_path', type=str, default='./data/w_pems_228.csv')
 parser.add_argument('--save_path', type=str, default='./model/save/')
-parser.add_argument('--in_timesteps', type=int, default=12)
-parser.add_argument('--out_timesteps', type=int, default=3)
-parser.add_argument('--train_days', type=int, default=34)
-parser.add_argument('--val_days', type=int, default=5)
-parser.add_argument('--test_days', type=int, default=5)
+parser.add_argument('--n_time', type=int, default=12)
+parser.add_argument('--out_time', type=int, default=3)
+parser.add_argument('--train_pct', type=float, default=0.7)
+parser.add_argument('--test_pct', type=float, default=0.2)
 
 args = parser.parse_args()
 
@@ -37,16 +35,16 @@ print("PyTorch Version: ", torch.__version__)
 data = TrafficFlowData(device=device,
                        v_path=args.v_path,
                        w_path=args.w_path,
-                       len_train=args.train_days * 288,
-                       len_val=args.val_days * 288,
-                       in_timesteps=args.in_timesteps,
-                       out_timesteps=args.out_timesteps)
+                       train_pct=args.train_pct,
+                       test_pct=args.test_pct,
+                       n_time=args.n_time,
+                       out_time=args.out_time)
 train_iter = DataLoader(data.train, args.batch_size)
 val_iter = DataLoader(data.val, args.batch_size)
 test_iter = DataLoader(data.test, args.batch_size)
 
 # Model Initialization
-model = STGCN(num_nodes=data.num_nodes, graph_conv_kernel=data.get_conv_kernel("Linear")).to(device)
+model = STGCN(num_nodes=data.n_node, graph_conv_kernel=data.get_conv_kernel("linear")).to(device)
 criterion = nn.MSELoss().to(device)
 optimizer = optim.RMSprop(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.decay_steps, gamma=args.decay_rate)

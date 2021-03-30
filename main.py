67daafcd8901cf748cecd951a3f43bde303c1b71
Yuexin Li <meetyuexinli@163.com>
2021-03-30 16:00:11 +0800

    add early stop mechanism for test.

--- main.py
+++ main.py
@@ -80,6 +80,7 @@ def train():
         if early_stop.check(val_loss):
             break
         if early_stop.save:
+            print()
             torch.save(model.state_dict(), args.save_path)
 
         print('Epoch: {:03d} | Lr: {:.20f} | Train loss: {:.6f} | Val loss: {:.6f}'.format(
